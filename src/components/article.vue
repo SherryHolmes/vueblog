a94a038134b1c9cb7102f073e92f6b79b5a73dff
xinzongyan <xinzongyan@qq.com>
2018-06-06 11:37:17 +0800

    Md编辑器

--- src/components/article.vue
+++ src/components/article.vue
@@ -1,41 +1,35 @@
 <template>
-  <div>
-    <Scroll :on-reach-bottom="handleReachBottom">
-      <Card dis-hover v-for="(item, index) in list1" :key="index" style="margin: 20px 0">
-        Content {{ item }}
-      </Card>
-    </Scroll>
-  </div>
+
+
+
 </template>
 
 
 <script>
 
+
   export default {
+
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App',
-        list1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        input: '# Input Markdown Code'
       }
     },
+    computed: {
+
+    },
     methods: {
-      handleReachBottom() {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            const last = this.list1[this.list1.length - 1];
-            for (let i = 1; i < 11; i++) {
-              this.list1.push(last + i);
-            }
-            resolve();
-          }, 2000);
-        });
-      }
+
+    },
+    components: {
+
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 
 
 </style>
--- src/components/other.vue
+++ src/components/other.vue
@@ -1,23 +1,71 @@
 <template >
   <div>
-
     <h1><a href="/Port(V3.0).zip" download="Port(V3.0).zip">小南瓜串口下载</a></h1>
+    <div class="editor">
+      <h2>Md编辑器</h2>
+      <!--<textarea :value="input" @input="update"></textarea>-->
+      <Input type="textarea" class="editarea"  v-model="input"  :autosize="{minRows: 9}"
+             placeholder="请输入留言信息..."> </Input>
+      <div v-html="compiledMarkdown"></div>
+    </div>
   </div>
 </template>
 
 
 <script>
+  import moMarkd from 'marked'
+  //  import moLodash from 'lodash'
   export default {
     name: 'mainother',
-    data () {
+    data() {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        input: '# Input Markdown Code'
       }
-    }
+    },
+    computed: {
+      compiledMarkdown: function () {
+        return moMarkd(this.input, {sanitize: true})
+      }
+    },
+    methods: {
+      /*update: _.debounce(function (e) {
+        this.input = e.target.value
+      }, 300)*/
+    },
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .editor {
+    margin: 10px 0;
+    font-family: 'Helvetica Neue', Arial, sans-serif;
+    color: #333;
 
+  }
+
+  .editarea ,
+  .editor div {
+    display: inline-block;
+    width: 49%;
+    height: 100%;
+    vertical-align: top;
+    box-sizing: border-box;
+
+  }
+
+  .editarea {
+    border: none;
+    border-right: 1px solid #ccc;
+    resize: none;
+    #outline: none;
+    background-color: #f6f6f6;
+    font-size: 14px;
+    font-family: 'Monaco', courier, monospace;
+
+
+  }
+  code {
+    color: #f66;
+  }
 </style>
